ec260e30767e96a0fbb02956f02548acea1d28cd
TechExplorerAnish <anishghimire178@gmail.com>
2025-07-06 01:01:05 +0545

    feat: add code store for managing source code, labels, and error states

---
+++ src/store/codeStore.js
@@ -0,0 +1,10 @@
+export const useCodeStore = create((set) => ({
+    sourceCode: "",
+    labels: {},
+    errors: [],
+    setSourceCode: (code) => set({ sourceCode: code }),
+    setLabels: (labels) => set({ labels }),
+    setErrors: (errors) => set({ errors }),
+    clearErrors: () => set({ errors: [] })
+  }));
+  
